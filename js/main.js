8623aa00f173c21d21dd5bfbeb3d88a1c8cfffa3
Ephraim Nann <ephnann@gmail.com>
2021-07-20 10:51:35 +0930

    Responsive navbar v1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
+const logomobile = document.querySelector(".logo-mobile");
+const logo = document.querySelector("#logo");
  
 /* Toggle mobile menu */
 function toggleMenu() {
@@ -13,6 +15,22 @@ function toggleMenu() {
          
         // adds the close (x) icon
         toggle.querySelector("a").innerHTML = "<span class='material-icons'>close</span>";
+    }
+    if (logomobile.classList.contains("active")) {
+        logomobile.classList.remove("active");
+        logomobile.classList.add("disactive");
+    } else {
+        logomobile.classList.add("active");
+        logomobile.classList.remove("disactive");
+    }
+    //Re-add logo here
+    if (logo.classList.contains("active")) {
+        logo.classList.add("active");
+        logo.classList.remove("disactive");
+        
+    } else {
+        logo.classList.remove("active");
+        logo.classList.add("disactive");
     }
 }
  
